331cb5a3be6e475d3a037841228b8f38aef3d405
joao-oliveira-neto <joaodabete@gmail.com>
2019-05-04 20:40:31 -0300

    contador

---
+++ contador/Crescente.java
@@ -0,0 +1,21 @@
+package contador;
+
+public class Crescente {
+
+	public static void main(String[] args) {
+		int contador_d=10;
+		int contador_c=-10;
+		while (contador_d >=-10)
+		while (contador_c <=10)	
+		{
+			System.out.print("|DECRESCENTE:" + contador_d);
+			contador_d--;
+			System.out.println("  |CRESCENTE:" + contador_c);
+			contador_c++;
+				
+		}
+		
+		
+	}
+
+}
---
+++ contador/CrescenteIi.java
@@ -0,0 +1,32 @@
+package contador;
+
+public class CrescenteIi {
+
+	public static void main(String[] args) {
+		int a =5;
+			int b = 3;
+			int c = 10;
+			if(a> b && a>c) {
+				if(b>c) {
+					System.out.println(a+b+c);
+				}
+				else if(c>b) {
+					System.out.println(a+c+b);
+				}
+			}
+			else if(b>a && b>c) {
+				 if(a>c) {
+					 System.out.println(b+a+c);
+				 }
+				 else if(c>a) {
+					 System.out.println(b+c+a);
+				 }
+			}
+			else if(c>a && c>b) {
+				if(a>b) {
+					 System.out.println(c+a+b);
+				}
+			}
+	}
+
+}
